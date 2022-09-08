f8488f72a2c7c9a7b9065cfb49c4dd92adf947a7
twcrone <twcrone@gmail.com>
2022-09-08 10:41:55 -0400

    Initial commit

---
+++ src/main/java/latecurio/ratpack/Main.java
@@ -0,0 +1,13 @@
+package latecurio.ratpack;
+
+import ratpack.server.RatpackServer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        RatpackServer.start(spec -> spec
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render("Hello, World!")))
+        );
+    }
+}
