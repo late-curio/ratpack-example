e9765952f84447941144d9b81795e603782e25e7
twcrone <twcrone@gmail.com>
2022-09-08 13:44:28 -0400

    Add tests to run for ~30 mins

--- test.sh
+++ test.sh
@@ -1,40 +1,14 @@
-http localhost:5050 name==Kevyn
-sleep 1
-http localhost:5050 name==André
-sleep 1
-http localhost:5050 name==Jason
-sleep 1
-http localhost:5050 name==Brad
-sleep 1
-http localhost:5050 name==Oren
-sleep 1
-http localhost:5050 name==Kevyn
-sleep 1
-http localhost:5050 name==André
-sleep 1
-http localhost:5050 name==Jason
-sleep 1
-http localhost:5050 name==Brad
-sleep 1
-http localhost:5050 name==Oren
-sleep 1
-http localhost:5050 name==Kevyn
-sleep 1
-http localhost:5050 name==André
-sleep 1
-http localhost:5050 name==Jason
-sleep 1
-http localhost:5050 name==Brad
-sleep 1
-http localhost:5050 name==Oren
-sleep 1
-http localhost:5050 name==Kevyn
-sleep 1
-http localhost:5050 name==André
-sleep 1
-http localhost:5050 name==Jason
-sleep 1
-http localhost:5050 name==Brad
-sleep 1
-http localhost:5050 name==Oren
-sleep 1
+# shellcheck disable=SC2034
+for i in {1..200}
+do
+  http localhost:5050 name==Kevyn
+  sleep 1
+  http localhost:5050 name==André
+  sleep 1
+  http localhost:5050 name==Jason
+  sleep 1
+  http localhost:5050 name==Brad
+  sleep 1
+  http localhost:5050 name==Oren
+  sleep 1
+done
