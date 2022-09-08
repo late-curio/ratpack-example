35bbbbc98bda3250860340b7f51fec1b7a1cd6b0
twcrone <twcrone@gmail.com>
2022-09-08 12:23:23 -0400

    Add HttpClient call

--- src/main/java/latecurio/ratpack/DefaultHandler.java
+++ src/main/java/latecurio/ratpack/DefaultHandler.java
@@ -1,15 +1,37 @@
 package latecurio.ratpack;
 
+import io.netty.buffer.PooledByteBufAllocator;
+import ratpack.exec.Promise;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
+import ratpack.http.client.HttpClient;
+import ratpack.http.client.ReceivedResponse;
+import ratpack.server.ServerConfig;
 
+import java.net.URI;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
 public class DefaultHandler implements Handler {
+    private final HttpClient httpClient;
+
+    public DefaultHandler() throws Exception {
+        this.httpClient = HttpClient.of(httpClientSpec -> httpClientSpec.poolSize(10)
+                .connectTimeout(Duration.of(60, ChronoUnit.SECONDS))
+                .maxContentLength(ServerConfig.DEFAULT_MAX_CONTENT_LENGTH)
+                .responseMaxChunkSize(16384)
+                .readTimeout(Duration.of(60, ChronoUnit.SECONDS))
+                .byteBufAllocator(PooledByteBufAllocator.DEFAULT));
+    }
+
     @Override
     public void handle(Context ctx) {
         Map<String, String> queryParams = ctx.getRequest().getQueryParams();
         String name = queryParams.get("name");
-        ctx.render(String.format("%s", name));
+        URI uri = URI.create("http://localhost:5050/hello?name=" + name);
+        Promise<ReceivedResponse> helloResponse = httpClient.get(uri);
+        helloResponse.map(receivedResponse -> receivedResponse.getBody().getText())
+                .then(responseText -> ctx.getResponse().send(responseText));
     }
 }
--- test.sh
+++ test.sh
@@ -1,40 +1,52 @@
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
-http localhost:5050/hello name==Ratpack
+http localhost:5050 name==Ratpack
+sleep 1
+http localhost:5050/goodbye name==Ratpack
+sleep 1
+http localhost:5050 name==Ratpack
+sleep 1
+http localhost:5050/goodbye name==Ratpack
+sleep 1
+http localhost:5050 name==Ratpack
+sleep 1
+http localhost:5050/goodbye name==Ratpack
+sleep 1
+http localhost:5050 name==Ratpack
 sleep 1
 http localhost:5050/goodbye name==Ratpack
 sleep 1
